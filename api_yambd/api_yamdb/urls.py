dfa9661b30e7d29dd7f227a085fb4b435e4d832c
Андрей Бычков <bimsuch@gmail.com>
2023-06-22 11:27:44 +0300

    1

--- api_yambd/api_yamdb/urls.py
+++ api_yambd/api_yamdb/urls.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from django.views.generic import TemplateView
 from django.urls import path, include
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path(
